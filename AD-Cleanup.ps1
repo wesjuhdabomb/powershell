4a235485b2249c9c409a7e483452bbc391903d5c
Wesley <w.vandamme@outlook.com>
2019-03-03 20:58:33 +0100

    test2

---
+++ AD-Cleanup.ps1
@@ -0,0 +1,8 @@
+Import-Module activedirectory
+
+#Find stale user accounts
+
+#Find stale computer accounts (based on lastlogon date)
+
+#Find empty groups, which do not follow the naming convention
+
